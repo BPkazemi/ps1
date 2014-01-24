40f325f83f867b644bff2a23d24e244dd1d2a08f
Babak Pourkazemi <bp5xj@virginia.edu>
2014-01-24 01:23:40 -0500

    zhttpto server

---
+++ answers.md
@@ -0,0 +1,10 @@
+Title: Problem Set 1 Answers
+Author: Babak Pourkazemi
+
+Problem 1.
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.77 Safari/537.36
+
+Explanation: I am using Chrome but the user agent is Mozilla/5.0. A quick SO search reveals this dates back to the Netscape/IE days. The parenthesis describe my computing environment - I am on a Macbook with Mavericks running an Intel chip. I would presume the AppleWebKit is some core functioning code for displaying web content. Chrome/32.0.1700.77 is my chrome version. I do not know why Safari is listed at the end.
+
+Problem 2.
+I recall either Professor Evans or the tutorial discussing how Rust is built to be very safe. It would make sense for global variables to need special permission to be changed to prevent a race condition, then. In other words, it is for concurrency safety's sake.
--- zhttpto.rs
+++ zhttpto.rs
@@ -14,13 +14,15 @@
 #[feature(globs)];
 use std::io::*;
 use std::io::net::ip::{SocketAddr};
+use std::io::buffered::BufferedReader;
 use std::{str};
 
 static IP: &'static str = "127.0.0.1";
 static PORT:        int = 4414;
+static mut counter: int = 0;
 
 fn main() {
-    let addr = from_str::<SocketAddr>(format!("{:s}:{:d}", IP, PORT)).unwrap();
+    let addr = from_str::<SocketAddr>(format!("{:s}:{:d}", IP, PORT)).unwrap(); // addr = IP + PORT
     let mut acceptor = net::tcp::TcpListener::bind(addr).listen();
     
     println(format!("Listening on [{:s}] ...", addr.to_str()));
@@ -44,17 +46,93 @@ fn main() {
             stream.read(buf);
             let request_str = str::from_utf8(buf);
             println(format!("Received request :\n{:s}", request_str));
-            
-            let response: ~str = 
-                ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
-                 <doctype !html><html><head><title>Hello, Rust!</title>
-                 <style>body { background-color: #111; color: #FFEEAA }
-                        h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-                        h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
-                 </style></head>
-                 <body>
-                 <h1>Greetings, Krusty!</h1>
-                 </body></html>\r\n";
+            unsafe {
+                counter += 1;
+                println("Number of visits: " + counter.to_str());
+            }
+
+            // Get the first line of the request
+            let mut host_index = 0;
+            while(true) {  
+                if(str::eq(&request_str.slice(host_index, host_index+4).to_owned(), &~"Host")) {
+                    break;
+                }
+                host_index += 1;
+            }
+            let restful = request_str.slice(0, host_index);
+
+            // Split the line into its three components and store them
+            let mut temp = ~[];
+            for s in restful.split(' ') {
+                temp.push(s.to_owned());
+            }
+
+            let http_type = temp[0].clone(); 
+            let path = temp[1].slice_from(1).to_owned().clone(); 
+            let http_meta = temp[2].slice(0, 8).to_owned().clone(); 
+
+            // TODO: Make sure the error page is okay. Something vague about this in the instructions
+
+            let mut path_ending = ~"";
+            if(path.len() > 4) {
+                path_ending = path.slice(path.len()-4, path.len()).to_owned();
+            }
+
+            let mut response: ~str = ~"";
+
+            // Check the validity of the HTTP params
+            if(path_ending != ~"html") {
+                response = 
+                    ~"HTTP/1.1 403 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                     <doctype !html><html><head><title>403</title>
+                     <style>body 
+                            h1 { font-size:2cm; text-align: center; color: black;}
+                     </style></head>
+                     <body>
+                     <h1>four, oh, three</h1>
+                     </body></html>\r\n";
+            } else {
+                if (http_type == ~"GET" && http_meta == ~"HTTP/1.1") {
+                    // Check the validity of the path
+                    if(path.len() == 0 || !&Path::new(path.clone()).is_file()) {
+                        response = 
+                        ~"HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                         <doctype !html><html><head><title>404</title>
+                         <style>body 
+                                h1 { font-size:2cm; text-align: center; color: black;}
+                         </style></head>
+                         <body>
+                         <h1>four, oh, four</h1>
+                         </body></html>\r\n";
+                    } else {
+                        let file = File::open(&Path::new(path.clone()));
+                        match (file) {
+                            Some(contents) => {
+                                let mut reader = BufferedReader::new(contents);
+                                for line in reader.lines() {
+                                    response = response + "\n" + line;
+                                }
+                            }
+                            None => {
+                            }
+                        }
+                    }
+                }
+                else {
+                    // Invalid HTTP params
+                    response = 
+                        ~"HTTP/1.1 403 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                         <doctype !html><html><head><title>404</title>
+                         <style>body 
+                                h1 { font-size:2cm; text-align: center; color: black;}
+                         </style></head>
+                         <body>
+                         <h1>four, oh, three</h1>
+                         </body></html>\r\n";
+                }
+            }
+
+
             stream.write(response.as_bytes());
             println!("Connection terminates.");
         }
